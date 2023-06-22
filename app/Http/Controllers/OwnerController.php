b91a9068464f71e9e20aef4bdf56ce8a0a4ea8f2
unknown <samuelop99op@gmail.com>
2023-06-22 10:34:56 -0700

    primera ruta de owners

--- app/Http/Controllers/OwnerController.php
+++ app/Http/Controllers/OwnerController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\owner;
 
 class OwnerController extends Controller
 {
     //
+
+    public function index()
+    {
+        $owners = Owner::with('devices')->get();
+        return response()->json($owners);
+    }
 }
