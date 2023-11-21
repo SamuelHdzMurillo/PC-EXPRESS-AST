1a6dfbb5fa8e0040845205e27db7412d89127385
samuel hernandez <samuelop99op@gmail.com>
2023-11-21 14:39:14 -0700

    landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,78 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PC Express - Soluciones Tecnológicas</title>
+  <style>
+    /* Estilos CSS para la página */
+    body {
+      font-family: Arial, sans-serif;
+      margin: 0;
+      padding: 0;
+      background-color: #f4f4f4;
+      color: #333;
+    }
+    header {
+      background-color: #007bff;
+      color: #fff;
+      text-align: center;
+      padding: 20px 0;
+    }
+    .container {
+      width: 80%;
+      margin: 20px auto;
+    }
+    .section {
+      margin-bottom: 30px;
+    }
+    .section h2 {
+      color: #007bff;
+    }
+    .section p {
+      line-height: 1.6;
+    }
+    footer {
+      background-color: #333;
+      color: #fff;
+      text-align: center;
+      padding: 10px 0;
+      position: fixed;
+      bottom: 0;
+      width: 100%;
+    }
+  </style>
 </head>
 <body>
-    <h1>HOLA MAMUKISS!!!!!</h1>
+  <header>
+    <h1>PC Express</h1>
+    <p>Con más de 30 años de experiencia en soluciones tecnológicas</p>
+  </header>
+
+  <div class="container">
+    <section class="section">
+      <h2>Nuestra Historia</h2>
+      <p>Desde nuestra fundación en 1992, PC Express ha sido pionero en ofrecer una amplia gama de servicios y productos informáticos. Iniciamos como una pequeña tienda de informática local y, a lo largo de los años, hemos evolucionado y crecido para convertirnos en líderes en el sector.</p>
+    </section>
+
+    <section class="section">
+      <h2>Nuestros Servicios</h2>
+      <p>Ofrecemos una selección diversa de productos y servicios informáticos, incluyendo hardware, soluciones empresariales, soporte técnico y desarrollo de software.</p>
+    </section>
+
+    <section class="section">
+      <h2>¿Por qué Elegirnos?</h2>
+      <p>Nuestros más de 30 años de experiencia, compromiso con la calidad y enfoque en el cliente nos convierten en tu mejor opción para soluciones tecnológicas.</p>
+    </section>
+
+    <section class="section">
+      <h2>Nuestros Clientes Satisfechos</h2>
+      <p>Los testimonios de nuestros clientes respaldan nuestro compromiso con la excelencia y la satisfacción del cliente. [Agrega aquí algunos testimonios o comentarios de clientes, si los tienes]</p>
+    </section>
+  </div>
+
+  <footer>
+    <p>&copy; 2023 PC Express - Todos los derechos reservados</p>
+  </footer>
 </body>
 </html>
