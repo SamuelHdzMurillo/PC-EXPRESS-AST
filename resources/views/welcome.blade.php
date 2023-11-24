e5e35f0f6c3c4576d9b33f253dc9a11b1d0a3e59
samuel hernandez <samuelop99op@gmail.com>
2023-11-24 15:22:20 -0700

    cambio a landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,78 +1,228 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>PC Express - Soluciones Tecnológicas</title>
-  <style>
-    /* Estilos CSS para la página */
-    body {
-      font-family: Arial, sans-serif;
-      margin: 0;
-      padding: 0;
-      background-color: #f4f4f4;
-      color: #333;
-    }
-    header {
-      background-color: #007bff;
-      color: #fff;
-      text-align: center;
-      padding: 20px 0;
-    }
-    .container {
-      width: 80%;
-      margin: 20px auto;
-    }
-    .section {
-      margin-bottom: 30px;
-    }
-    .section h2 {
-      color: #007bff;
-    }
-    .section p {
-      line-height: 1.6;
-    }
-    footer {
-      background-color: #333;
-      color: #fff;
-      text-align: center;
-      padding: 10px 0;
-      position: fixed;
-      bottom: 0;
-      width: 100%;
-    }
-  </style>
+    <meta charset="UTF-8">
+    <title>PC Express - Landing Page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        /* Estilos adicionales */
+        body {
+            background-color: #121212;
+            color: #fff;
+        }
+
+        .navbar {
+            background-color: #0d6efd !important;
+        }
+
+        .jumbotron {
+            background-color: #0d6efd;
+            color: #fff;
+        }
+
+        .card {
+            background-color: #1f1f1f;
+            color: #fff;
+        }
+
+        .card-title {
+            color: #17a2b8;
+        }
+
+        .card-text {
+            color: #ddd;
+        }
+        .half-and-half {
+            display: flex;
+            align-items: center;
+            background: #1f1f1f;
+            margin: 2%;
+            padding: 10px;
+        }
+        .half-and-half .img-fluid {
+            max-width: 100%;
+            height: auto;
+        }
+        
+    </style>
 </head>
+
 <body>
-  <header>
-    <h1>PC Express</h1>
-    <p>Con más de 30 años de experiencia en soluciones tecnológicas</p>
-  </header>
+    <!-- Barra de navegación -->
+    
+    <!-- Contenido principal -->
+    <div class="container mt-4">
+        <!-- Encabezado -->
+        <header class="jumbotron text-center">
+            <h1 class="display-4">Bienvenido a PC Express</h1>
+            <p class="lead">Encuentra los mejores productos y servicios para tu PC.</p>
+        </header>
 
-  <div class="container">
-    <section class="section">
-      <h2>Nuestra Historia</h2>
-      <p>Desde nuestra fundación en 1992, PC Express ha sido pionero en ofrecer una amplia gama de servicios y productos informáticos. Iniciamos como una pequeña tienda de informática local y, a lo largo de los años, hemos evolucionado y crecido para convertirnos en líderes en el sector.</p>
-    </section>
+        
 
-    <section class="section">
-      <h2>Nuestros Servicios</h2>
-      <p>Ofrecemos una selección diversa de productos y servicios informáticos, incluyendo hardware, soluciones empresariales, soporte técnico y desarrollo de software.</p>
-    </section>
+        <!-- Sección de servicios -->
+        <div class="row half-and-half">
+            <div class="col-md-6">
+                <div class="p-4">
+                    <h2>Ofrecemos servicios de alta calidad</h2>
+                    <p>
+                        En PC EXPRESS nos dedicamos a brindar soluciones tecnológicas avanzadas para satisfacer las necesidades de nuestros clientes.
+                        Ofrecemos servicios de instalación, mantenimiento y soporte técnico para hogares y empresas.
+                    </p>
+                    <p>
+                        Contamos con un equipo altamente capacitado para proporcionar servicios de vigilancia, instalación de sistemas de punto de venta, suministro de consumibles y más.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <img src="https://imgs.search.brave.com/BuUKcWo-fcDOIoZ2BUyWN8Jw2KDvR1RzlXUKdci1TOo/rs:fit:860:0:0/g:ce/aHR0cHM6Ly90aGVu/ZXR3b3JraW5zdGFs/bGVycy5jb20vd3At/Y29udGVudC91cGxv/YWRzLzIwMjIvMDQv/TmV0d29yay1JbnN0/YWxsYXRpb24tRW5n/aW5lZXItMTAyNHg2/ODMuanBn" alt="Imagen de Servicios" class="img-fluid">
+            </div>
+        </div>
 
-    <section class="section">
-      <h2>¿Por qué Elegirnos?</h2>
-      <p>Nuestros más de 30 años de experiencia, compromiso con la calidad y enfoque en el cliente nos convierten en tu mejor opción para soluciones tecnológicas.</p>
-    </section>
+        <!-- Sección de productos destacados -->
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <img src="https://m.media-amazon.com/images/I/61PkGRSw05L._AC_SL1500_.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Laptop Dell </h5>
+                        <p class="card-text">
+Dell Latitude 5400 Business Laptop, 14 pulgadas FHD (1920 x 1080) no táctil, Quad Core 8ª generación i5-8365U, 16 GB RAM, 512 GB SSD</p>
+                        <a href="#" class="btn btn-primary">$7,500</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <img src="https://m.media-amazon.com/images/I/51Ikux-sZaL._AC_SL1000_.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Laptop Lenovo</h5>
+                        <p class="card-text">
+Lenovo Thinkpad T470s Laptop empresarial - 20HF0012US 14 FHD, i5-7300U 2.6GHz, 8 GB DDR4 RAM, 256 GB SSD</p>
+                        <a href="#" class="btn btn-primary">$5,600</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <img src="https://m.media-amazon.com/images/I/61mvi+Jxo4L._AC_SL1500_.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Laptop Dell</h5>
+                        <p class="card-text">Dell Latitude E5470 14" FHD Laptop, Core i7-6820HQ 2.7GHz, 16GB, 512GB unidad de estado sólido, Windows 10 Pro 64Bit</p>
+                        <a href="#" class="btn btn-primary">$7,500</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container mt-5">
+        <h1 class="text-center mb-4">Servicios de Soporte Técnico</h1>
 
-    <section class="section">
-      <h2>Nuestros Clientes Satisfechos</h2>
-      <p>Los testimonios de nuestros clientes respaldan nuestro compromiso con la excelencia y la satisfacción del cliente. </p>
-    </section>
-  </div>
+        
 
-  <footer>
-    <p>&copy; 2023 PC Express - Todos los derechos reservados</p>
-  </footer>
+        <section>
+            <h2>Servicios Ofrecidos</h2>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="card mb-3">
+                        <div class="card-body">
+                            <h5 class="card-title">Antivirus</h5>
+                            <p class="card-text">Instalación de antivirus por $100 pesos.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card mb-3">
+                        <div class="card-body">
+                            <h5 class="card-title">Instalación de Office</h5>
+                            <p class="card-text">Instalación de Microsoft Office por $100 pesos.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card mb-3">
+                        <div class="card-body">
+                            <h5 class="card-title">Formateo</h5>
+                            <p class="card-text">Formateo de equipos por $350 pesos.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="card mb-3">
+                        <div class="card-body">
+                            <h5 class="card-title">Mantenimiento de pc</h5>
+                            <p class="card-text">Mantenimiento de pc equipos por $350 pesos.</p>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </section>
+        
+
+        <!-- Formulario de contacto -->
+        <section class="mb-5">
+            <h2 class="text-center mb-4">Contáctanos</h2>
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                <div class="form-group">
+                    <label for="name">Nombre</label>
+                    <input type="text" class="form-control" id="name" name="name" required>
+                </div>
+                <div class="form-group">
+                    <label for="email">Correo electrónico</label>
+                    <input type="email" class="form-control" id="email" name="email" required>
+                </div>
+                <div class="form-group">
+                    <label for="message">Mensaje</label>
+                    <textarea class="form-control" id="message" name="message" rows="4" required></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Enviar mensaje</button>
+            </form>
+        </section>
+    </div>
+
+    <div class="container mt-5">
+        <h1 class="text-center mb-4">PC EXPRESS - Sucursal Principal</h1>
+
+        <!-- Contenedor del mapa -->
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14572.646484887257!2d-110.3036889!3d24.0606124!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86afcd69069c8363%3A0xa69a0672411d9159!2sPC%20EXPRESS!5e0!3m2!1ses-419!2smx!4v1700860704163!5m2!1ses-419!2smx" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>            </div>
+        </div>
+    <footer class="text-center py-4">
+        <p>&copy; <?php echo date("Y"); ?> PC Express. Todos los derechos reservados.</p>
+        <p>Del Agua e/Av. La Fuente y Cto.</p>
+        <p>La Paz, B.C.S, México , CP 23088</p>
+    </footer>
+    <!-- PHP para enviar correos -->
+    <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $recipient_email = "gerenciapcexpressbcs@gmail.com"; // Cambia esto por tu dirección de correo electrónico
+            $subject = "Nuevo mensaje desde el formulario de contacto";
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $message = $_POST['message'];
+
+            $email_content = "Nombre: $name\n";
+            $email_content .= "Correo electrónico: $email\n\n";
+            $email_content .= "Mensaje:\n$message";
+
+            $headers = "From: $name <$email>";
+
+            if (mail($recipient_email, $subject, $email_content, $headers)) {
+                echo "<p class='alert alert-success'>¡El mensaje ha sido enviado con éxito!</p>";
+            } else {
+                echo "<p class='alert alert-danger'>Error al enviar el mensaje. Por favor, inténtalo de nuevo más tarde.</p>";
+            }
+        }
+        ?>
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    
 </body>
+
 </html>
