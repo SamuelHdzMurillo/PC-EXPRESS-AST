e50d431232f0b3111f45c838864594c1cda767ee
samuel hernandez <samuelop99op@gmail.com>
2023-12-04 11:09:53 -0700

    cambio a imagen del banner y agregado de icon en el navegador

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>PcExpress - Soluciones Tecnologicas</title>
+    <link rel="icon" href="{{ asset('img/PC LOGO PX.png') }}" />
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
       rel="stylesheet"
@@ -190,7 +191,7 @@
         </div>
         <div class="carousel-item">
           <img
-            src="{{ asset('img/DESCUENTO.gif') }}"
+            src="{{ asset('img/bannerpromo.gif') }}"
             class="d-block w-100"
             alt="Placeholder Image 2"
           />
